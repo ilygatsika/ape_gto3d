4ed46c98569321ba9dcb2346963d08f122cec34d
ilygatsika <ioligatsika@outlook.com>
2024-04-06 19:15:20 +0200

    update test

--- example/estimator_01.sh
+++ example/estimator_01.sh
@@ -4,10 +4,10 @@
 #   >>> nohup ./example/estimator_01 > out/estimator_01/out.log &
 
 OUT_DIR=out/estimator_01 
-#BASES="cc-pvdz unc-cc-pvdz unc-cc-pvtz pc-1 unc-pc-1 pc-2 unc-pc-2 \
-#    cc-pvtz aug-cc-pvdz aug-cc-pvtz aug-cc-pvqz cc-pvqz cc-pv5z \
-#    aug-cc-pv5z pc-3 pc-4 aug-pc-3 aug-pc-4 unc-pc-4"
-BASES="cc-pvdz unc-cc-pvdz unc-cc-pvtz pc-1"
+BASES="cc-pvdz unc-cc-pvdz unc-cc-pvtz pc-1 unc-pc-1 pc-2 unc-pc-2 \
+    cc-pvtz aug-cc-pvdz aug-cc-pvtz aug-cc-pvqz cc-pvqz cc-pv5z \
+    aug-cc-pv5z pc-3 pc-4 aug-pc-3 aug-pc-4 unc-pc-4"
+#BASES="cc-pvdz unc-cc-pvdz unc-cc-pvtz pc-1"
 
 for BASIS in $BASES;
 do 
