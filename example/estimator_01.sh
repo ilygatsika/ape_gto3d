ef95457d49510b2c2c04709fa6a205e4edb46e8d
ilygatsika <ioligatsika@outlook.com>
2024-04-06 19:12:56 +0200

    merge ljll server

--- example/estimator_01.sh
+++ example/estimator_01.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 #   >>> nohup ./example/estimator_01.sh > out/estimator_01/out.log &
-=======
 #   >>> nohup ./example/estimator_01 > out/estimator_01/out.log &
->>>>>>> e724d646a150cccb7dc63a70aaeccb7fe9245990
 
 OUT_DIR=out/estimator_01 
 #BASES="cc-pvdz unc-cc-pvdz unc-cc-pvtz pc-1 unc-pc-1 pc-2 unc-pc-2 \
@@ -14,11 +11,7 @@ BASES="cc-pvdz unc-cc-pvdz unc-cc-pvtz pc-1"
 
 for BASIS in $BASES;
 do 
-<<<<<<< HEAD
     python3 main.py $BASIS $OUT_DIR
-=======
-    $PYTHON main.py $BASIS $OUT_DIR
->>>>>>> e724d646a150cccb7dc63a70aaeccb7fe9245990
 done
 
 git add out/estimator_01
