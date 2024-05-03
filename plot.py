e6cd4e3f4b58688e65f95fa84846f24d3b95db7f
Ioanna-Maria Lygatsika <ioligatsika@outlook.com>
2024-05-03 10:48:20 +0200

    estimator plot

--- plot.py
+++ plot.py
@@ -1,3 +1,44 @@
+import matplotlib.pyplot as plt
+import pickle
+import numpy as np
+
+with open("res.pickle", 'rb') as file:
+	data = pickle.load(file)
+
+basis = list(data.keys())
+n_bas = len(basis)
+s = data[basis[0]]["shift"]
+s1 = data[basis[0]]["shift1"]
+s2 = data[basis[0]]["shift2"]
+s3 = data[basis[0]]["shift3"]
+shift = (s1, s2, s3, s)
+print(shift)
+
+estim_atom = np.array([data[basis[i]]["estim_atom"] for i in range(n_bas)])
+estim_Delta = np.array([data[basis[i]]["estim_Delta"] for i in range(n_bas)])
+estim = np.array([data[basis[i]]["estimator"] for i in range(n_bas)])
+err_H = np.array([data[basis[i]]["err_H"] for i in range(n_bas)])
+
+# Sort with desceanding error
+idx = np.argsort(err_H)[::-1]
+
+# Plot error convergence
+plt.title("s_1=%.1f s_2=%.1f s_inf=%.1f s=%.1f" %shift)
+labels = [basis[idx[i]] for i in range(n_bas)]
+plt.xticks(np.arange(n_bas), labels, rotation=45, fontsize=8, ha='right', rotation_mode='anchor')
+plt.plot(estim[idx], 'x-', label=r"total estimator")
+plt.plot(estim_atom[idx], 'x-', label=r"atom term")
+plt.plot(estim_Delta[idx], 'x-', label=r"inf term")
+plt.plot(err_H[idx], '^-', label=r"$u_1 - u_{1N}$ in H")
+plt.yscale("log")
+plt.legend()
+plt.tight_layout()
+plt.savefig("norm.pdf")
+plt.close()
+
+exit()
+
+
 # Plot
 from math import exp, log
 import numpy as np
